d6ee71c4bbe59a3c2273be06d2b8ffdcccc902ea
GuGuitar <kamonputh.duangmanee@g.swu.ac.th>
2026-04-24 23:06:22 +0700

    logo

--- index.php
+++ index.php
@@ -24,7 +24,10 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top shadow-sm">
   <div class="container">
-    <a class="navbar-brand fw-bold" href="index.php">Internships System</a>
+    <a class="navbar-brand fw-bold d-flex align-items-center" href="index.php">
+  <img src="https://supreme.swu.ac.th/img/logo_color.png" alt="Logo" width="30" height="30" class="me-2">
+  Internships System
+</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
       <span class="navbar-toggler-icon"></span>
     </button>
