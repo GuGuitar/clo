928b4dc7d76f0750e705a893595ebe31024f7de5
iamjuadu <juadu.ped@gmail.com>
2026-04-25 16:57:30 +0700

    Add User database

---
+++ users_202604251654.sql
@@ -0,0 +1,38 @@
+INSERT INTO internships.users (username,password,`role`,first_name,last_name,title,year_level,profile_image,education) VALUES
+	 ('admin','admin','staff','Admin','Krub',NULL,NULL,NULL,NULL),
+	 ('teacher01','1234','teacher','ดิษฐ์','สุทธิวงศ์','อ.ดร.',NULL,'Dit-scaled.jpg','ปริญญาเอก: Ph.D. Information Technology , มหาวิทยาลัยพระจอมเกล้าพระนครเหนือ\\n
+ปริญญาโท: วท.ม. การจัดการเทคโนโลยีสารสนเทศ , มหาวิทยาลัยหอการค้าไทย\\n
+ปริญญาตรี: วท.บ. ศาสตร์คอมพิวเตอร์ , มหาวิทยาลัยธรรมศาสตร์'),
+	 ('6610001','1234','student','Somchai','Jaidee',NULL,3,NULL,NULL),
+	 ('6610002','1234','student','Somsri','Rukthai',NULL,4,NULL,NULL),
+	 ('6610003','1234','student','Mana','Manee',NULL,3,NULL,NULL),
+	 ('6610004','1234','student','Piti','Chujai',NULL,4,NULL,NULL),
+	 ('6610005','1234','student','Weera','Suwan',NULL,3,NULL,NULL),
+	 ('6610006','1234','student','Napa','Deemak',NULL,4,NULL,NULL),
+	 ('6610007','1234','student','Komsan','Kengkan',NULL,3,'',NULL),
+	 ('6610008','1234','student','Suda','Ngamta',NULL,4,NULL,NULL);
+INSERT INTO internships.users (username,password,`role`,first_name,last_name,title,year_level,profile_image,education) VALUES
+	 ('6610009','1234','student','Wichai','Phoprasert',NULL,3,NULL,NULL),
+	 ('6610010','1234','student','Naree','Rakrien',NULL,4,NULL,NULL),
+	 ('teacher02','1234','teacher','ฐิติ','อติชาติชยากร','อ.ดร.',NULL,'thiti-scaled.jpg','ปริญญาเอก: ค.ด. เทคโนโลยีและสื่อสารการศึกษา, จุฬาลงกรณ์มหาวิทยาลัย\\n
+ปริญญาโท: อ.ม. บรรณารักษศาสตร์และสารนิเทศศาสตร์, จุฬาลงกรณ์มหาวิทยาลัย\\n
+ปริญญาตรี: ศศ.บ. บรรณารักษศาสตร์และสารสนเทศศาสตร์, มหาวิทยาลัยศรีนครินทรวิโรฒ'),
+	 ('teacher03','1234','teacher','วิภากร','วัฒนสินธุ์','ผศ.ดร.',NULL,'Vipakorn-200x300.jpg','ปริญญาเอก: วท.ด. ธุรกิจเทคโนโลยีและการจัดการนวัตกรรม, จุฬาลงกรณ์มหาวิทยาลัย\\n
+ปริญญาโท: MS Computer Information System, Colorado State University\\n
+ปริญญาโท: MBA Finance, Colorado State University\\n
+ปริญญาตรี: น.บ. นิติศาสตร์, มหาวิทยาลัยสุโขทัยธรรมาธิราช\\n
+ปริญญาตรี: บธ.บ การตลาด, จุฬาลงกรณ์มหาวิทยาลัย'),
+	 ('teacher04','1234','teacher','โชคธำรงค์','จงจอหอ','อ.ดร.',NULL,'Chokthamrong.jpg','ปรืญญาเอก: ปร.ด. สำรสนเทศศึกษำ, สำรสนเทศศึกษำ\\n
+ปริญญาโท: วท.ม. เทคโนโลยีผู้ประกอบกำรและกำรจัดกำรนวัตกรรม, มหำวิทยำลัยนเรศวร\\n
+ปรืญญาโท: ศศ.ม. กำรจัดกำรทรัพยำกรชีวภำพ, มหำวิทยำลัยเทคโนโลยีพระจอมเกล้ำธนบุรี\\n
+ปริญญาตรี: ศศ.บ. สำรสนเทศศำสตร์, มหำวิทยำลัยขอนแก่น'),
+	 ('teacher05','1234','teacher','โชติมา','วัฒนะ','อ.',NULL,'Chotima.jpg','ปริญญาเอก: Ph.D. Philosophy Information Science, Sukhothai Thammathirat Open University \\n
+ปริญญาโท: M.A. Information Management, KhonKean University\\n
+ปริญญาตรี: B.A. Information Science, Mahasarakham University'),
+	 ('teacher06','1234','teacher','ดุษฎี','สีวังคำ','ผศ.ดร.',NULL,'Dussadee-683x1024.jpg','ปริญญาเอก: Ph.D. Technical Education Technology, King Mongkut’s University of Technology North Bangkok\\n
+ปริญญาโท: M.S. Information Technology,  King Mongkut’s University of Technology North Bangkok\\n
+ปริญญาตรี	: B.S. Computer Science, Rajabhat Institute Phetchabun'),
+	 ('teacher07','1234','teacher','ศศิพิมล','ประพินพงศกร','ผศ.ดร.',NULL,'Sasipimol-683x1024.jpg','ปริญญาเอก: ค.ด. เทคโนโลยีแยีละสื่อสื่สารการศึกษา, จุฬาลงกรณ์มณ์ หาวิทวิ ยาลัย\\n'),
+	 ('teacher08','1234','teacher','ศุมรรษตรา','แสนวา','อ.ดร.',NULL,'Sumattra-683x1024.jpg','ปริญญาเอก: ปร.ด. สารสนเทศศึกษา , มหาวิทยาลัยขอนแก่น \\n
+ปริญญาโท: ศศ.ม. บรรณารักษศาสตร์และสารสนเทศศาสตร์ , มหาวิทยาลัยมหาสารคาม\\n
+ปริญฯาตรี: ศศ.บ. ภาษาอังกฤษ, มหาวิทยาลัยมหาสารคาม');
