c35c10e4000b0964fdf4b5136fbd51936866c261
iamjuadu <juadu.ped@gmail.com>
2026-04-25 17:18:11 +0700

    Update New Database

--- database.sql
+++ database.sql
@@ -51,30 +51,46 @@ CREATE TABLE `evaluations` (
 -- Insert Mock Data
 -- Password is '1234' for everyone. For production, apply hashing like password_hash(), but for this exercise plain or simple hash is fine. We will use plain for simplicity as requested 'Password: 1234'.
 
--- Staff
-INSERT INTO `users` (`username`, `password`, `role`, `first_name`, `last_name`, `title`, `year_level`, `profile_image`, `education`) VALUES
-('admin', '1234', 'staff', 'Admin', 'Krub', NULL, NULL, NULL, NULL),
-('teacher01', '1234', 'teacher', 'สมปอง', 'สุขสวัสดิ์', 'อ.ดร.', NULL, NULL, 'ปริญญาเอก: Ph.D. Computer Science, Carnegie Mellon University\nปริญญาโท: M.Sc. Software Engineering, Chulalongkorn University\nปริญญาตรี: B.Sc. Computer Science, มหาวิทยาลัยศรีนครินทรวิโรฒ'),
-('teacher02', '1234', 'teacher', 'วิภาวดี', 'ศรีสุวรรณ', 'ผศ.ดร.', NULL, NULL, 'ปริญญาเอก: Ph.D. Information Technology, King Mongkut''s University of Technology Thonburi\nปริญญาโท: M.Sc. Computer Engineering, Kasetsart University\nปริญญาตรี: B.Sc. Information Technology, มหาวิทยาลัยศรีนครินทรวิโรฒ'),
-('teacher03', '1234', 'teacher', 'ประเสริฐ', 'เจริญสุข', 'อ.ดร.', NULL, NULL, 'ปริญญาเอก: Ph.D. Data Science, University of Tokyo\nปริญญาโท: M.Eng. Computer Engineering, Chulalongkorn University\nปริญญาตรี: B.Eng. Computer Engineering, มหาวิทยาลัยเกษตรศาสตร์'),
-('teacher04', '1234', 'teacher', 'นภาพร', 'รักเรียน', 'ผศ.ดร.', NULL, NULL, 'ปริญญาเอก: Ph.D. Artificial Intelligence, Stanford University\nปริญญาโท: M.Sc. Computer Science, Thammasat University\nปริญญาตรี: B.Sc. Mathematics, มหาวิทยาลัยศรีนครินทรวิโรฒ'),
-('teacher05', '1234', 'teacher', 'กิตติพงศ์', 'แก้วมณี', 'อ.ดร.', NULL, NULL, 'ปริญญาเอก: Ph.D. Cybersecurity, University of Melbourne\nปริญญาโท: M.Sc. Information Security, Mahidol University\nปริญญาตรี: B.Sc. Computer Science, จุฬาลงกรณ์มหาวิทยาลัย'),
-('teacher06', '1234', 'teacher', 'สุชาดา', 'พงศ์ไพบูลย์', 'ผศ.ดร.', NULL, NULL, 'ปริญญาเอก: Ph.D. Software Engineering, National University of Singapore\nปริญญาโท: M.Sc. Software Engineering, มหาวิทยาลัยศรีนครินทรวิโรฒ\nปริญญาตรี: B.Sc. Information Technology, มหาวิทยาลัยธรรมศาสตร์'),
-('teacher07', '1234', 'teacher', 'ธนวัฒน์', 'อุดมโชค', 'อ.ดร.', NULL, NULL, 'ปริญญาเอก: Ph.D. Machine Learning, ETH Zurich\nปริญญาโท: M.Eng. Electrical Engineering, King Mongkut''s Institute of Technology Ladkrabang\nปริญญาตรี: B.Eng. Computer Engineering, มหาวิทยาลัยเกษตรศาสตร์'),
-('teacher08', '1234', 'teacher', 'พิมพ์ชนก', 'วงศ์สกุล', 'ผศ.ดร.', NULL, NULL, 'ปริญญาเอก: Ph.D. Human-Computer Interaction, MIT\nปริญญาโท: M.Sc. UX Design, Chulalongkorn University\nปริญญาตรี: B.Sc. Computer Science, มหาวิทยาลัยศรีนครินทรวิโรฒ');
+-- Users (Staff, Teachers, Students)
+INSERT INTO `users` (`username`,`password`,`role`,`first_name`,`last_name`,`title`,`year_level`,`profile_image`,`education`) VALUES
+	 ('admin','admin','staff','Admin','Krub',NULL,NULL,NULL,NULL),
+	 ('teacher01','1234','teacher','ดิษฐ์','สุทธิวงศ์','อ.ดร.',NULL,'Dit-scaled.jpg','ปริญญาเอก: Ph.D. Information Technology , มหาวิทยาลัยพระจอมเกล้าพระนครเหนือ\\n
+ปริญญาโท: วท.ม. การจัดการเทคโนโลยีสารสนเทศ , มหาวิทยาลัยหอการค้าไทย\\n
+ปริญญาตรี: วท.บ. ศาสตร์คอมพิวเตอร์ , มหาวิทยาลัยธรรมศาสตร์'),
+	 ('6610001','1234','student','Somchai','Jaidee',NULL,3,NULL,NULL),
+	 ('6610002','1234','student','Somsri','Rukthai',NULL,4,NULL,NULL),
+	 ('6610003','1234','student','Mana','Manee',NULL,3,NULL,NULL),
+	 ('6610004','1234','student','Piti','Chujai',NULL,4,NULL,NULL),
+	 ('6610005','1234','student','Weera','Suwan',NULL,3,NULL,NULL),
+	 ('6610006','1234','student','Napa','Deemak',NULL,4,NULL,NULL),
+	 ('6610007','1234','student','Komsan','Kengkan',NULL,3,'',NULL),
+	 ('6610008','1234','student','Suda','Ngamta',NULL,4,NULL,NULL);
 
--- Students (10 mock)
-INSERT INTO `users` (`username`, `password`, `role`, `first_name`, `last_name`, `title`, `year_level`, `profile_image`, `education`) VALUES
-('6610001', '1234', 'student', 'Somchai', 'Jaidee', NULL, 3, NULL, NULL),
-('6610002', '1234', 'student', 'Somsri', 'Rukthai', NULL, 4, NULL, NULL),
-('6610003', '1234', 'student', 'Mana', 'Manee', NULL, 3, NULL, NULL),
-('6610004', '1234', 'student', 'Piti', 'Chujai', NULL, 4, NULL, NULL),
-('6610005', '1234', 'student', 'Weera', 'Suwan', NULL, 3, NULL, NULL),
-('6610006', '1234', 'student', 'Napa', 'Deemak', NULL, 4, NULL, NULL),
-('6610007', '1234', 'student', 'Komsan', 'Kengkan', NULL, 3, NULL, NULL),
-('6610008', '1234', 'student', 'Suda', 'Ngamta', NULL, 4, NULL, NULL),
-('6610009', '1234', 'student', 'Wichai', 'Phoprasert', NULL, 3, NULL, NULL),
-('6610010', '1234', 'student', 'Naree', 'Rakrien', NULL, 4, NULL, NULL);
+INSERT INTO `users` (`username`,`password`,`role`,`first_name`,`last_name`,`title`,`year_level`,`profile_image`,`education`) VALUES
+	 ('6610009','1234','student','Wichai','Phoprasert',NULL,3,NULL,NULL),
+	 ('6610010','1234','student','Naree','Rakrien',NULL,4,NULL,NULL),
+	 ('teacher02','1234','teacher','ฐิติ','อติชาติชยากร','อ.ดร.',NULL,'thiti-scaled.jpg','ปริญญาเอก: ค.ด. เทคโนโลยีและสื่อสารการศึกษา, จุฬาลงกรณ์มหาวิทยาลัย\\n
+ปริญญาโท: อ.ม. บรรณารักษศาสตร์และสารนิเทศศาสตร์, จุฬาลงกรณ์มหาวิทยาลัย\\n
+ปริญญาตรี: ศศ.บ. บรรณารักษศาสตร์และสารสนเทศศาสตร์, มหาวิทยาลัยศรีนครินทรวิโรฒ'),
+	 ('teacher03','1234','teacher','วิภากร','วัฒนสินธุ์','ผศ.ดร.',NULL,'Vipakorn-200x300.jpg','ปริญญาเอก: วท.ด. ธุรกิจเทคโนโลยีและการจัดการนวัตกรรม, จุฬาลงกรณ์มหาวิทยาลัย\\n
+ปริญญาโท: MS Computer Information System, Colorado State University\\n
+ปริญญาโท: MBA Finance, Colorado State University\\n
+ปริญญาตรี: น.บ. นิติศาสตร์, มหาวิทยาลัยสุโขทัยธรรมาธิราช\\n
+ปริญญาตรี: บธ.บ การตลาด, จุฬาลงกรณ์มหาวิทยาลัย'),
+	 ('teacher04','1234','teacher','โชคธำรงค์','จงจอหอ','อ.ดร.',NULL,'Chokthamrong.jpg','ปริญญาเอก: ปร.ด. สารสนเทศศึกษา, สารสนเทศศึกษา\\n
+ปริญญาโท: วท.ม. เทคโนโลยีผู้ประกอบการและการจัดการนวัตกรรม, มหาวิทยาลัยนเรศวร\\n
+ปริญญาโท: ศศ.ม. การจัดการทรัพยากรชีวภาพ, มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี\\n
+ปริญญาตรี: ศศ.บ. สารสนเทศศาสตร์, มหาวิทยาลัยขอนแก่น'),
+	 ('teacher05','1234','teacher','โชติมา','วัฒนะ','อ.',NULL,'Chotima.jpg','ปริญญาเอก: Ph.D. Philosophy Information Science, Sukhothai Thammathirat Open University \\n
+ปริญญาโท: M.A. Information Management, KhonKean University\\n
+ปริญญาตรี: B.A. Information Science, Mahasarakham University'),
+	 ('teacher06','1234','teacher','ดุษฎี','สีวังคำ','ผศ.ดร.',NULL,'Dussadee-683x1024.jpg','ปริญญาเอก: Ph.D. Technical Education Technology, King Mongkut’s University of Technology North Bangkok\\n
+ปริญญาโท: M.S. Information Technology,  King Mongkut’s University of Technology North Bangkok\\n
+ปริญญาตรี	: B.S. Computer Science, Rajabhat Institute Phetchabun'),
+	 ('teacher07','1234','teacher','ศศิพิมล','ประพินพงศกร','ผศ.ดร.',NULL,'Sasipimol-683x1024.jpg','ปริญญาเอก: ค.ด. เทคโนโลยีและสื่อสารการศึกษา, จุฬาลงกรณ์มหาวิทยาลัย\\n'),
+	 ('teacher08','1234','teacher','ศุมรรษตรา','แสนวา','อ.ดร.',NULL,'Sumattra-683x1024.jpg','ปริญญาเอก: ปร.ด. สารสนเทศศึกษา , มหาวิทยาลัยขอนแก่น \\n
+ปริญญาโท: ศศ.ม. บรรณารักษศาสตร์และสารสนเทศศาสตร์ , มหาวิทยาลัยมหาสารคาม\\n
+ปริญญาตรี: ศศ.บ. ภาษาอังกฤษ, มหาวิทยาลัยมหาสารคาม');
 
 -- Mock Internship Request
 INSERT INTO `internship_requests` (`student_id`, `company_name`, `position`, `start_date`, `end_date`, `status`) VALUES
