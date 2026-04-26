cfc1ec8a7203dbf362c84813f1bbc25554b03431
GuGuitar <kamonputh.duangmanee@g.swu.ac.th>
2026-04-26 18:23:00 +0700

    3

--- save_student.php
+++ save_student.php
@@ -5,15 +5,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
-    $year_level = $_POST['year_level'];
+    $student_year = $_POST['student_year'];
     $role = 'student';
-    $password = '1234';
 
-    $sql = "INSERT INTO users (username, password, first_name, last_name, year_level, role) 
-            VALUES ('$username', '$password', '$first_name', '$last_name', '$year_level', '$role')";
+    $sql = "INSERT INTO users (username, first_name, last_name, student_year, role) 
+            VALUES ('$username', '$first_name', '$last_name', '$student_year', '$role')";
 
     if ($conn->query($sql) === TRUE) {
-        echo "<script>alert('บันทึกข้อมูลสำเร็จ'); window.location='student_list.php?year=$year_level';</script>";
+        echo "<script>alert('บันทึกข้อมูลสำเร็จ'); window.location='student_list.php?year=$student_year';</script>";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
